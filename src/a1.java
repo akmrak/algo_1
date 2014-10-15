21b99172613367459f3c2475c03fce6f2322c631
akmrak <abhishek.kmr.ak@gmail.com>
2014-10-15 23:33:37 +0530

    Final verified full marks

--- src/a1.java
+++ src/a1.java
@@ -4,9 +4,9 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class a1 {
-    public static int mergeSort(int[] arrayList,int[] C,int p1, int p2)
+    public static long mergeSort(int[] arrayList,int[] C,int p1, int p2)
     {
-        int n=0;
+        long n=0;
         if(p2==p1)
             return 0;
         else if(p2>p1)
@@ -24,9 +24,9 @@ public class a1 {
         return n;
 
     }
-    public static int merge(int[] A,int[] C, int l , int m , int r )
+    public static long merge(int[] A,int[] C, int l , int m , int r )
     {
-        int n=0;
+        long n=0;
         int k=l;
         int i=l,j=m+1;
         while(i<=m&&j<=r)
@@ -110,18 +110,18 @@ public class a1 {
                 ex.printStackTrace();
             }
         }
-        int[] arrayList1={1,2 ,3 ,4 ,5 ,7, 6 ,8, 9,8};
+        int[] arrayList1= { 4, 80, 70, 23, 9, 60, 68, 27, 66, 78, 12, 40, 52, 53, 44, 8, 49, 28, 18, 46, 21, 39, 51, 7, 87, 99, 69, 62, 84, 6, 79, 67, 14, 98, 83, 0, 96, 5, 82, 10, 26, 48, 3, 2, 15, 92, 11, 55, 63, 97, 43, 45, 81, 42, 95, 20, 25, 74, 24, 72, 91, 35, 86, 19, 75, 58, 71, 47, 76, 59, 64, 93, 17, 50, 56, 94, 90, 89, 32, 37, 34, 65, 1, 73, 41, 36, 57, 77, 30, 22, 13, 29, 38, 16, 88, 61, 31, 85, 33, 54 };
         int[] C1=new int[arrayList1.length];
-        System.out.println(mergeSort(arrayList1,C1,0,arrayList1.length-1));
+        System.out.println(mergeSort(arrayList,C,0,arrayList.length-1));
         int[] arrayList2={12,23,34,45,48,2,9,24,36,47};
         //merge(arrayList2,C1,0,4,9);
         int t=0;
-        while(t<arrayList1.length)
-        {
-            System.out.println(arrayList1[t]);
-            t++;
+        //while(t<arrayList1.length)
+        //{
+        //    System.out.println(arrayList1[t]);
+        //    t++;
 
-        }
+        //}
 
     }
 }
